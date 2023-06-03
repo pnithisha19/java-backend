a42c4a65fc6b1d3ea2cb23df6ee97aaf85e011fc
pnithisha19 <nithishapamarthy@gmail.com>
2023-06-03 14:32:45 -0500

    Changes has been made related to Class Object Example

---
+++ com/examples/OOPS/Vehicle.java
@@ -0,0 +1,13 @@
+package com.examples.OOPS;
+
+public class Vehicle {
+    public void start()
+    {
+        System.out.println("Vehicle started");
+    }
+
+    public void stop()
+    {
+        System.out.println("Vehicle stopped");
+    }
+}
---
+++ com/examples/VehicleMain.java
@@ -0,0 +1,12 @@
+package com.examples;
+
+import com.examples.OOPS.Vehicle;
+
+public class VehicleMain {
+    public static void main(String[] args)
+    {
+        Vehicle car = new Vehicle();
+        car.start();
+        car.stop();
+    }
+}

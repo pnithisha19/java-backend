c1ca69ae0079c7a6111ee84445cd01e8f83b0eaf
pnithisha19 <nithishapamarthy@gmail.com>
2023-06-03 10:56:18 -0500

    My First Program

--- com/examples/First.java
+++ com/examples/First.java
@@ -3,7 +3,7 @@ package com.examples;
 public class First {
     public static void main (String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("My First Program");
     }
 
 }
